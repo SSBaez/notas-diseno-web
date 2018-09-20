60592eb8e8d49ac93c669339062be00ff3f045e2
Saúl Báez <ss.baez@comunidad.unam.mx>
2018-09-19 23:57:51 -0500

    primer commit

---
+++ Prueba.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ocultar</title>
+  </head>
+  <body>
+    <div class="contenedor">
+      <?php
+        $visible = "text";
+        echo "<h2>Hola</h2>";
+        echo "<input type:hidden name:'edad' value:"/Jose"/</input>"
+      ?>
+    </div>
+  </body>
+</html>
